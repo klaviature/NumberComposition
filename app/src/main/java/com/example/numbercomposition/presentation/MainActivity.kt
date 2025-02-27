a14097c74919e8363278eaa41aa270c4f8660768
egork <egorfomin8158@gmail.com>
2025-02-27 19:29:10 +0500

    Add presentation package

--- app/src/main/java/com/example/numbercomposition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.numbercomposition
+package com.example.numbercomposition.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.numbercomposition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
